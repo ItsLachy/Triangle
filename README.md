6b9b5e534b1f411bb9cae42cb69a310e4d18bd01
ethamitc <ethamitc@gmail.com>
2020-10-29 20:53:23 -0400

    Initial Commit 2.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Triangle
+# Triangle [![Discord](https://img.shields.io/discord/760646632584511539?style=flat-square&label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/weTkeRB)
 Multi-purpose Discord bot
---
+++ src/Triangle.ts
@@ -0,0 +1,20 @@
+import * as Sentry from '@sentry/node';
+
+import Config from './config.json';
+
+if (Config.sentryUrl) {
+    Sentry.init({
+        dsn: Config.sentryUrl,
+        release: 'Triangle@' + process.env.npm_package_version,
+        environment: process.env.PRODUCTION ? 'production' : 'development',
+        integrations: [
+            new Sentry.Integrations.Modules(),
+            new Sentry.Integrations.FunctionToString(),
+            new Sentry.Integrations.LinkedErrors(),
+            new Sentry.Integrations.Console(),
+        ],
+        tracesSampleRate: 1.0,
+    });
+}
+
+throw new Error('Test Error');
